30cbfbd864a5ae006c45688a1b7d5d303d14e341
OpportunityLiu <opportunity@live.in>
2019-04-25 20:13:44 +0800

    auto fill

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { EhTagConnectorService } from 'src/services/eh-tag-connector.service';
 import { RouteService } from 'src/services/route.service';
-import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest, merge, of, interval, timer } from 'rxjs';
 import { isValidRaw, editableNs, NamespaceInfo, ETKey } from 'src/interfaces/ehtranslation';
 import { MatSnackBar } from '@angular/material';
 import { FormControl, Validators, FormGroup, AbstractControl, ValidationErrors } from '@angular/forms';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, repeat, repeatWhen, delay, flatMap, catchError } from 'rxjs/operators';
 import * as Bluebird from 'bluebird';
 import { TitleService } from 'src/services/title.service';
 import { GithubOauthService } from 'src/services/github-oauth.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { snackBarConfig } from 'src/environments/environment';
 import { Tag, NamespaceName, NamespaceEnum } from 'src/interfaces/ehtag';
+import { GithubReleaseService } from 'src/services/github-release.service';
 type Fields = keyof Tag<'raw'> | keyof ETKey;
 interface Item extends Tag<'raw'>, ETKey { }
 
@@ -45,6 +46,7 @@ export class EditorComponent implements OnInit {
   constructor(
     private ehTagConnector: EhTagConnectorService,
     public github: GithubOauthService,
+    public release: GithubReleaseService,
     private router: RouteService,
     private title: TitleService,
     private snackBar: MatSnackBar, ) {
@@ -73,22 +75,23 @@ export class EditorComponent implements OnInit {
   create: Observable<boolean>;
   inputs: {
     namespace: Observable<NamespaceName | null>;
+    raw: Observable<string | null>;
+    name: Observable<string | null>;
+    intro: Observable<string | null>;
+    links: Observable<string | null>;
+  };
+
+  original: {
+    namespace: Observable<NamespaceName>;
     raw: Observable<string>;
     name: Observable<string>;
     intro: Observable<string>;
     links: Observable<string>;
   };
 
-  original: {
-    namespace: Observable<NamespaceName>;
-    raw: Observable<string>;
-    // name: Observable<string>;
-    // intro: Observable<string>;
-    // links: Observable<string>;
-  };
-
   rendered = {
     loading: new BehaviorSubject<boolean>(false),
+    raw: new BehaviorSubject<string>(''),
     name: new BehaviorSubject<string>(''),
     intro: new BehaviorSubject<string>(''),
     links: new BehaviorSubject<string>(''),
@@ -99,18 +102,41 @@ export class EditorComponent implements OnInit {
   narrowPreviewing = false;
 
   ngOnInit() {
+    const ons = this.router.initParam('namespace',
+      v => v && v in NamespaceEnum ? v as NamespaceName : 'artist');
+    const oraw = this.router.initParam('raw',
+      v => { v = (v || '').trim(); return isValidRaw(v) ? v : ''; },
+      v => {
+        if (v) {
+          this.title.setTitle('修改标签 - ' + v);
+        } else {
+          this.title.setTitle('新建标签');
+        }
+      });
+    const otag = combineLatest([
+      ons,
+      oraw,
+      merge(
+        of(undefined),
+        this.release.getCachedTags('raw'),
+        timer(0, 30000).pipe(flatMap(i => this.release.getTags('raw').pipe(catchError(_ => this.release.getCachedTags('raw')))))
+      )
+    ]).pipe(map(data => {
+      if (!data[2]) {
+        return undefined;
+      }
+      const nsdata = data[2].data.find(ns => ns.namespace === data[0]);
+      if (!nsdata) {
+        return undefined;
+      }
+      return nsdata.data[data[1]];
+    }));
     this.original = {
-      namespace: this.router.initParam('namespace',
-        v => v && v in NamespaceEnum ? v as NamespaceName : 'artist'),
-      raw: this.router.initParam('raw',
-        v => { v = (v || '').trim(); return isValidRaw(v) ? v : ''; },
-        v => {
-          if (v) {
-            this.title.setTitle('修改标签 - ' + v);
-          } else {
-            this.title.setTitle('新建标签');
-          }
-        }),
+      namespace: ons,
+      raw: oraw,
+      name: otag.pipe(map(t => t && t.name || '')),
+      intro: otag.pipe(map(t => t && t.intro || '')),
+      links: otag.pipe(map(t => t && t.links || '')),
     };
     this.create = this.original.raw.pipe(map(v => !isValidRaw(v)), tap(v => {
       if (v) {
@@ -125,24 +151,23 @@ export class EditorComponent implements OnInit {
       namespace: this.router.initQueryParam('namespace',
         v => v && v in NamespaceEnum ? v as NamespaceName : null),
       raw: this.router.initQueryParam('raw',
-        v => (v || '')),
+        v => v),
       name: this.router.initQueryParam('name',
-        v => (v || ''),
-        v => v ? this.getControl('name').setValue(v) : null),
+        v => v),
       intro: this.router.initQueryParam('intro',
-        v => (v || ''),
-        v => v ? this.getControl('intro').setValue(v) : null),
+        v => v),
       links: this.router.initQueryParam('links',
-        v => (v || ''),
-        v => v ? this.getControl('links').setValue(v) : null),
+        v => v),
     };
     for (const key in this.tagForm.controls) {
       if (this.tagForm.controls.hasOwnProperty(key)) {
         const element = this.tagForm.controls[key];
         element.valueChanges.subscribe(value => {
-          this.router.navigateParam({
-            [key]: value
-          });
+          if (element.dirty) {
+            this.router.navigateParam({
+              [key]: value
+            });
+          }
         });
       }
     }
@@ -150,8 +175,19 @@ export class EditorComponent implements OnInit {
       .pipe(map(v => v[0] ? (v[2] || v[1]) : v[1]))
       .subscribe(v => v ? this.getControl('namespace').setValue(v) : null);
     combineLatest([this.create, this.original.raw, this.inputs.raw])
-      .pipe(map(v => v[0] ? (v[2] || v[1]) : v[1]))
+      .pipe(map(v => v[0] ? (v[2] || v[1]) : v[1]), tap(v => this.rendered.raw.next(v)))
       .subscribe(v => v ? this.getControl('raw').setValue(v) : null);
+
+    // 不要使用 v[1] || v[0]，v[1] === '' 表示已经编辑
+    combineLatest([this.original.name, this.inputs.name])
+      .pipe(map(v => v[1] === null ? v[0] : v[1]))
+      .subscribe(v => v ? this.getControl('name').setValue(v) : null);
+    combineLatest([this.original.intro, this.inputs.intro])
+      .pipe(map(v => v[1] === null ? v[0] : v[1]))
+      .subscribe(v => v ? this.getControl('intro').setValue(v) : null);
+    combineLatest([this.original.links, this.inputs.links])
+      .pipe(map(v => v[1] === null ? v[0] : v[1]))
+      .subscribe(v => v ? this.getControl('links').setValue(v) : null);
   }
 
   private getControl(field: Fields | null) {
