f1beed5654dea36cb046b4c5990c5f8fc5effdc8
Opportunity <opportunity@live.in>
2021-09-07 13:25:09 +0800

    docs: add docs

--- src/shared/interfaces/ehtag.ts
+++ src/shared/interfaces/ehtag.ts
@@ -1,8 +1,8 @@
 import type * as Ast from './ehtag.ast';
 import type { Opaque } from 'type-fest';
 
+/** 表示一个 SHA1 字符串，用来记录 Git 版本 */
 export type Sha1Value = Opaque<string, Commit>;
-
 export const Sha1Value = Object.freeze(
     Object.assign(
         (value: string): Sha1Value | undefined => {
@@ -21,12 +21,14 @@ export const Sha1Value = Object.freeze(
     ),
 );
 
+/** 表示一个 Git 签名 */
 export interface Signature {
     name: string;
     email: string;
     when: Date;
 }
 
+/** 表示一个 Git 提交 */
 export interface Commit {
     author: Signature;
     committer: Signature;
@@ -34,6 +36,7 @@ export interface Commit {
     message: string;
 }
 
+/** 表示一个 Git Repo 的信息 */
 export interface RepoInfo {
     repo: string;
     head: Commit;
@@ -41,10 +44,12 @@ export interface RepoInfo {
     data: NamespaceInfo[];
 }
 
+/** 表示一个 Git Repo 的数据 */
 export interface RepoData<T> extends RepoInfo {
     data: Array<NamespaceData<T>>;
 }
 
+/** 命名空间的完整名称 */
 export const NamespaceName = [
     'rows',
     'reclass',
@@ -59,33 +64,46 @@ export const NamespaceName = [
 ] as const;
 export type NamespaceName = typeof NamespaceName[number];
 
+/** 表示一个命名空间的信息 */
+
 export interface NamespaceInfo {
+    /** 包含的记录条数 */
     count: number;
     namespace: NamespaceName;
     frontMatters: FrontMatters;
 }
 
+/** 表示命名空间文件头部数据 */
 export interface FrontMatters {
     key: NamespaceName;
+    /** 命名空间中文名称 */
     name: string;
+    /** 命名空间描述 */
     description: string;
+    /** 内容版权信息 */
     copyright?: string;
+    /** 内容书写规则 */
     rules?: string[];
+    /** 示例 */
     example?: Tag<'raw'> & { raw: string };
 }
 
+/** 表示一个命名空间的数据 */
 export interface NamespaceData<T = TagType> extends NamespaceInfo {
     data: { [raw: string]: Tag<T> };
 }
 
+/** 表示一条翻译的内容 */
 export interface Tag<T = TagType> {
     name: CellType<T>;
     intro: CellType<T>;
     links: CellType<T>;
 }
 
+/** 翻译数据序列化的类型 */
 export type TagType = 'raw' | 'ast' | 'html' | 'text' | 'full';
 
+/** 表示一条翻译的单元格内容 */
 export type CellType<T> = T extends 'raw'
     ? string
     : T extends 'ast'
