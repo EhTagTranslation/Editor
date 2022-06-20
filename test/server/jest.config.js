63cfb04fedd8c372e6dedb6d7da7c59be94b7f26
Opportunity <opportunity@live.in>
2022-06-20 18:52:36 +0800

    test: fix coverage

--- test/server/jest.config.js
+++ test/server/jest.config.js
@@ -6,23 +6,18 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 /** @typedef {import('ts-jest')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-    //  moduleFileExtensions: ['js', 'json', 'ts'],
-    // rootDir: path.resolve(__dirname, '../..'),
-    // roots: ['<rootDir>/'],
-    setupFilesAfterEnv: ['./setup-jest.js'],
-    // moduleNameMapper: {
-    //     '^shared/.*$': '<rootDir>/src/$0',
-    //     '^server/.*$': '<rootDir>/src/$0',
-    // },
+    rootDir: path.resolve(__dirname, '../..'),
+    roots: ['<rootDir>/'],
+    setupFilesAfterEnv: [path.resolve(__dirname, './setup-jest.js')],
     testEnvironment: 'node',
-    testRegex: '(\\.test|\\.spec)\\.js$',
+    testRegex: 'test/server/.+(\\.test|\\.spec)\\.js$',
     transform: {},
 
     collectCoverage: true,
     coverageProvider: 'v8',
 
     moduleNameMapper: {
-        '^#(.*)$': '<rootDir>/../../dist/$1.js',
+        '^#(.*)$': '<rootDir>/dist/$1.js',
     },
 };
 
