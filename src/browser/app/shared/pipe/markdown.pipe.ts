09d9f41eb16e9b345ecd7b0b86c717c80a9dd223
OpportunityLiu <opportunity@live.in>
2023-03-14 11:38:37 +0800

    fix: markdown

--- src/browser/app/shared/pipe/markdown.pipe.ts
+++ src/browser/app/shared/pipe/markdown.pipe.ts
@@ -1,18 +1,15 @@
 import { parse, render } from '#shared/markdown';
-import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
     name: 'markdown',
     pure: true,
 })
 export class MarkdownPipe implements PipeTransform {
-    constructor(private readonly sanitizer: DomSanitizer) {}
-
-    transform(value: string | null): SafeHtml {
+    transform(value: string | null): string {
         value ??= '';
         const ast = parse(value, undefined);
         const html = render(ast, 'html');
-        return this.sanitizer.bypassSecurityTrustHtml(html);
+        return html;
     }
 }
