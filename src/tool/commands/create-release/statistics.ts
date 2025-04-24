98d13b5badc64c21c9d5fecbe8752287a67d9335
OpportunityLiu <opportunity@live.in>
2025-04-25 00:12:00 +0800

    feat: 优化统计信息输出格式

--- src/tool/commands/create-release/statistics.ts
+++ src/tool/commands/create-release/statistics.ts
@@ -4,13 +4,13 @@ export function withStatistics(value: string): string {
     if (STATISTICS.tagSuggest === 0 && STATISTICS.galleryList === 0 && STATISTICS.tagSearch === 0) return value;
     const phases = [value];
     if (STATISTICS.tagSuggest > 0) {
-        phases.push(`调用标签建议 ${STATISTICS.tagSuggest} 次`);
+        phases.push(`标签建议 ${STATISTICS.tagSuggest} 次`);
     }
     if (STATISTICS.galleryList > 0) {
-        phases.push(`调用图库信息 ${STATISTICS.galleryList} 次`);
+        phases.push(`图库信息 ${STATISTICS.galleryList} 次`);
     }
     if (STATISTICS.tagSearch > 0) {
         phases.push(`标签搜索 ${STATISTICS.tagSearch} 次`);
     }
-    return phases.join('，');
+    return `调用` + phases.join('，');
 }
