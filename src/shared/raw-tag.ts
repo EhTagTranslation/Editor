dce42c5fee35e9487a0b6ce627c4c63d803a0e5b
OpportunityLiu <opportunity@live.in>
2025-09-16 20:56:54 +0800

    fix: regex flag

--- src/shared/raw-tag.ts
+++ src/shared/raw-tag.ts
@@ -9,7 +9,7 @@ export function isRawTag(tag: unknown): tag is RawTag {
     if (typeof tag != 'string') return false;
     if (tag.length < 1) return false;
     if (tag.startsWith(' ') || tag.endsWith(' ')) return false;
-    if (!/^[-a-z0-9. ]+$/g.test(tag)) return false;
+    if (!/^[-a-z0-9. ]+$/.test(tag)) return false;
     return true;
 }
 
@@ -35,7 +35,7 @@ export function IsRawTag(validationOptions?: ValidationOptions): PropertyDecorat
                     if (tag.length < 1) return `${eachPrefix}$property is too short.`;
                     if (tag.startsWith(' ') || tag.endsWith(' '))
                         return `${eachPrefix}$property starts with or end with white spaces.`;
-                    if (!/^[-a-z0-9. ]$/g.test(tag))
+                    if (!/^[-a-z0-9. ]$/.test(tag))
                         return `${eachPrefix}$property included non-alphanumeric characters which are not permitted. Only hyphens, periods, and spaces are allowed in tags.`;
                     return `${eachPrefix}$property is not a valid tag.`;
                 }, validationOptions),
