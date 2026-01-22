4ce150c70d3478bf36536c62682fc6eb6c35d5d9
OpportunityLiu <opportunity@live.in>
2026-01-22 13:53:21 +0800

    build: update deps

--- src/browser/services/debug.service.ts
+++ src/browser/services/debug.service.ts
@@ -1,15 +1,13 @@
 import { Injectable, isDevMode } from '@angular/core';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class DebugService {
-    log(...args: Parameters<typeof console.log>): void {
+    log(...args: unknown[]): void {
         if (isDevMode()) {
             console.log(...args);
         }
     }
-    error(...args: Parameters<typeof console.error>): void {
+    error(...args: unknown[]): void {
         if (isDevMode()) {
             console.error(...args);
         }
