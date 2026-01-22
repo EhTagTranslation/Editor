0c36b96ee45ad8a26d713e32d3e96cbc0ae4e12a
OpportunityLiu <opportunity@live.in>
2026-01-22 18:19:37 +0800

    fix: swagger ui

--- src/server/setup.ts
+++ src/server/setup.ts
@@ -1,9 +1,14 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import type { INestApplication } from '@nestjs/common';
-import type { FastifyReply } from 'fastify';
+import type { FastifyReply, FastifyRequest } from 'fastify';
 
 export function setupSwagger(app: INestApplication): void {
-    const options = new DocumentBuilder()
+    const adapter = app.getHttpAdapter();
+    adapter.get('/', (req: FastifyRequest, res: FastifyReply) => {
+        void res.redirect('/swagger', 302);
+    });
+
+    const config = new DocumentBuilder()
         .setTitle('Eh Tag Translation Server')
         .addBearerAuth(undefined, 'GitHub-Token')
         .setDescription('连接到 EhTagTranslation 数据库的 RESTful API。')
@@ -11,10 +16,13 @@ export function setupSwagger(app: INestApplication): void {
         .addTag('Database', 'EhTagTranslation 数据库内容的增改删查。')
         .addTag('Tools', '不直接操作数据库的帮助工具。')
         .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('/swagger', app, document);
-    app.getHttpAdapter().get('/', (req, res: FastifyReply) => {
-        void res.redirect('/swagger', 302);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/swagger', app, document, {
+        customCssUrl: [`https://unpkg.com/swagger-ui-dist@5/swagger-ui.css`],
+        customJs: [
+            `https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js`,
+            `https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js`,
+        ],
     });
 }
 
